48cfc0aae14983ee5b96b2fa3d0fad5e4873bba6
Santos yosmar rios torrez <133970496+riossantos32@users.noreply.github.com>
2025-08-27 20:19:59 -0600

    Update firebaseconfig.jsx

--- src/database/firebaseconfig.jsx
+++ src/database/firebaseconfig.jsx
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage";
 
 // Configuración de Firebase usando variables de entorno
 const firebaseConfig = {
-  apiKey: "AIzaSyAAe8pO3B1r6XblPbSMQejCwcIp2wfdwYk",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
